8af64ea5b97b1050cdede2385a7cae1ca5838dcc
GEC\phongsathon.kon <phongsathon.kon@gec.co.th>
2018-05-28 19:34:49 +0700

    [TASK] alter tracjing items

---
+++ src/main/resources/db/migration/mssql/V5__alter_tracking_item.sql
@@ -0,0 +1,2 @@
+ALTER TABLE tbl_batch_tracking_items ALTER COLUMN reference_no varchar(255) NULL;
+ALTER TABLE tbl_batch_tracking_items ALTER COLUMN transaction_no varchar(50) NULL;
---
+++ src/main/resources/db/migration/mysql/V5__alter_tracking_item.sql
@@ -0,0 +1,2 @@
+ALTER TABLE tbl_batch_tracking_items MODIFY reference_no varchar(255) null;
+ALTER TABLE tbl_batch_tracking_items MODIFY transaction_no varchar(50) null;
